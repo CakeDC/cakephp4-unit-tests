98fc45af701c1a6d3875381529ed84da89ac9322
Jorge González <steinkel@gmail.com>
2017-06-08 01:30:53 -0400

    joining a tournament

--- src/Controller/TournamentsController.php
+++ src/Controller/TournamentsController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Network\Exception\MethodNotAllowedException;
 
 /**
  * Tournanents Controller
@@ -17,5 +18,19 @@ class TournamentsController extends AppController
         //display tournaments, and status
         $tournaments = $this->Tournaments->find('index', ['userId' => $this->Auth->user('id')]);
         $this->set(compact('tournaments'));
+    }
+
+    public function join($tournamentId)
+    {
+        $this->request->allowMethod(['post', 'put']);
+        if (!$this->request->is('ajax')) {
+            throw new MethodNotAllowedException();
+        }
+        $tournament = $this->Tournaments->get($tournamentId);
+        $tournament = $this->Tournaments->patchEntity($tournament, $this->request->getData());
+        $tournament['tournament_memberships'][0]['user_id'] = $this->Auth->user('id');
+        $this->Tournaments->save($tournament);
+        $this->set(compact('tournament'));
+        $this->render('/Element/Tournaments/join');
     }
 }
