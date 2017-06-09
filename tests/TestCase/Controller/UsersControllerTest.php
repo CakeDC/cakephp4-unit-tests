71578eecdda62c7bfc897fcd02f47e542e65e559
Jorge González <steinkel@gmail.com>
2017-06-08 21:49:26 -0400

    add failing registration test case

--- tests/TestCase/Controller/UsersControllerTest.php
+++ tests/TestCase/Controller/UsersControllerTest.php
@@ -51,4 +51,29 @@ class UsersControllerTest extends IntegrationTestCase
         $this->assertSame($email, $user['email']);
     }
 
+    /**
+     * Test register method
+     *
+     * @return void
+     */
+    public function testRegisterDuplicatedEmail()
+    {
+        $email = 'admin@example.com';
+        $usersTable = TableRegistry::get('Users');
+        $this->assertCount(1, $usersTable->findByEmail($email));
+        $data = [
+            'first_name' => 'John',
+            'last_name' => 'Smith',
+            'email' => $email,
+            'password' => 'password',
+        ];
+        $this->enableRetainFlashMessages();
+        $this->post('/users/register', $data);
+        $this->assertResponseSuccess();
+        $this->assertNoRedirect();
+        $this->assertSame('Unable to register the user.', Hash::get($this->_flashMessages, 'flash.0.message'));
+        $query = $usersTable->findByEmail($email);
+        $this->assertCount(1, $query);
+        $this->assertSame('john', $query->firstOrFail()->get('first_name'));
+    }
 }
