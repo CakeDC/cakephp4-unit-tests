522288eae42993e60fe157b264f7978ee45c6100
Jorge González <steinkel@gmail.com>
2020-04-10 11:02:05 +0100

    #unit-tests-code add integration tests for register action

--- tests/TestCase/Controller/UsersControllerTest.php
+++ tests/TestCase/Controller/UsersControllerTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 namespace App\Test\TestCase\Controller;
 
 use App\Controller\UsersController;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
+use Cake\Utility\Hash;
 
 /**
  * App\Controller\UsersController Test Case
@@ -37,14 +40,51 @@ class UsersControllerTest extends TestCase
         $this->markTestIncomplete('Not implemented yet.');
     }
 
-    /**
-     * Test register method
-     *
-     * @return void
-     */
-    public function testRegister(): void
+    public function testRegister()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+        $email = 'bill@example.com';
+        $usersTable = TableRegistry::getTableLocator()->get('Users');
+        $this->assertCount(0, $usersTable->findByEmail($email));
+        $data = [
+            'first_name' => 'Bill',
+            'last_name' => 'Smith',
+            'email' => $email,
+            'password' => 'password',
+        ];
+        $this->post('/users/register', $data);
+        $this->assertResponseSuccess();
+        $this->assertRedirect('/');
+        $user = $usersTable->findByEmail($email)->firstOrFail();
+        $hasher = new DefaultPasswordHasher();
+        $this->assertTrue($hasher->check('password', $user['password']));
+        $this->assertTrue($user['is_active']);
+        $this->assertTrue($user['is_superuser']);
+        $this->assertSame('Bill', $user['first_name']);
+        $this->assertSame('Smith', $user['last_name']);
+        $this->assertSame($email, $user['email']);
+    }
+
+    public function testRegisterDuplicatedEmail()
+    {
+        $this->enableCsrfToken();
+        $email = 'test@example.com';
+        $usersTable = TableRegistry::getTableLocator()->get('Users');
+        $this->assertCount(1, $usersTable->findByEmail($email));
+        $data = [
+            'first_name' => 'John',
+            'last_name' => 'Smith',
+            'email' => $email,
+            'password' => 'password',
+        ];
+        $this->enableRetainFlashMessages();
+        $this->post('/users/register', $data);
+        $this->assertResponseSuccess();
+        $this->assertNoRedirect();
+        $this->assertSame('Unable to register the user.', Hash::get($this->_flashMessages, 'flash.0.message'));
+        $query = $usersTable->findByEmail($email);
+        $this->assertCount(1, $query);
+        $this->assertSame('test', $query->firstOrFail()->get('first_name'));
     }
 
     /**
