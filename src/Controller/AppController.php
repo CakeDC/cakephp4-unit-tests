43638f1e685503ecd7235890b51b2be212a052e6
Jorge González <steinkel@gmail.com>
2020-05-18 13:14:33 +0100

    fix cs

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -46,6 +46,11 @@ class AppController extends Controller
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
         $this->loadComponent('Authentication.Authentication');
+//        $this->loadComponent('Authorization.Authorization', [
+//            'skipAuthorization' => [
+//                'login',
+//            ]
+//        ]);
         /*
          * Enable the following components for recommended CakePHP security settings.
          * see http://book.cakephp.org/3.0/en/controllers/components/security.html
